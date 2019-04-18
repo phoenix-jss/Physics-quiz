c3b11edf17fc221c994d5a3af762dfb5574cdc9f
abc <rajat2502@users.noreply.github.com>
2019-04-19 03:34:47 +0530

    quiz logic fixed

--- index.js
+++ index.js
@@ -1,84 +1,41 @@
 var quiz = {
     questions : ["Are u fucked up in life?","No. of days in a week?","No of mounths in a year?","Captian of indian cricket team?","Who played guddu in Mirzapur?"],
-    options : [["Yes","No","Not Sure","Hapily fucked up"],["2","7","5","6"],["8","10","12","11"],["MS Dhoni","V Kholi","Rohit Sharma","S Raina"],["Ali faizal","Srk","Salman","Varun"]],
+    opt1 : ["Yes","2","8","MS Dhoni"],
+    opt2 : ["No","7","10","V Kholi"],
+    opt3 : ["Not Sure","5","12","Rohit Sharma"],
+    opt4 : ["Hapily fucked up","6","11","S Raina"],
     answer : ["Hapily fucked up","7","12","V Kholi","Ali faizal"]
 };
+
+var i = 0;
+
 var score = 0;
-var index =0;
 
+document.getElementById("score").innerHTML = 0;
+document.getElementById("question").innerHTML = quiz.questions[i];
+document.getElementById("op1").innerHTML = quiz.opt1[i];
+document.getElementById("op2").innerHTML = quiz.opt2[i];
+document.getElementById("op3").innerHTML = quiz.opt3[i];
+document.getElementById("op4").innerHTML = quiz.opt4[i];
 
-if (index===0) {
-    var element = document.getElementById("question");
-    element.innerHTML= quiz.questions[0];
-    var opt1 = document.getElementById("op1");
-    opt1.innerHTML = quiz.options[0][0];
-    var opt2 = document.getElementById("op2");
-    opt2.innerHTML = quiz.options[0][1];
-    var opt3 = document.getElementById("op3");
-    opt3.innerHTML = quiz.options[0][2];
-    var opt4 = document.getElementById("op4");
-    opt4.innerHTML = quiz.options[0][3];
-
+function changeContent(){
+  document.getElementById("score").innerHTML = 0;
+  document.getElementById("question").innerHTML = quiz.questions[i];
+  document.getElementById("op1").innerHTML = quiz.opt1[i];
+  document.getElementById("op2").innerHTML = quiz.opt2[i];
+  document.getElementById("op3").innerHTML = quiz.opt3[i];
+  document.getElementById("op4").innerHTML = quiz.opt4[i];
 }
 
-else if (index===1) {
-    var element = document.getElementById("question");
-    element.innerHTML= quiz.questions[1];
-    var opt1 = document.getElementById("op1");
-    opt1.innerHTML = quiz.options[1][0];
-    var opt2 = document.getElementById("op2");
-    opt2.innerHTML = quiz.options[1][1];
-    var opt3 = document.getElementById("op3");
-    opt3.innerHTML = quiz.options[1][2];
-    var opt4 = document.getElementById("op4");
-    opt4.innerHTML = quiz.options[1][3];
-
-}
-
-else if (index===2) {
-    var element = document.getElementById("question");
-    element.innerHTML= quiz.questions[2];
-    var opt1 = document.getElementById("op1");
-    opt1.innerHTML = quiz.options[2][0];
-    var opt2 = document.getElementById("op2");
-    opt2.innerHTML = quiz.options[2][1];
-    var opt3 = document.getElementById("op3");
-    opt3.innerHTML = quiz.options[2][2];
-    var opt4 = document.getElementById("op4");
-    opt4.innerHTML = quiz.options[2][3];
-
-}
-
-else if (index===3) {
-    var element = document.getElementById("question");
-    element.innerHTML= quiz.questions[3];
-    var opt1 = document.getElementById("op1");
-    opt1.innerHTML = quiz.options[3][0];
-    var opt2 = document.getElementById("op2");
-    opt2.innerHTML = quiz.options[3][1];
-    var opt3 = document.getElementById("op3");
-    opt3.innerHTML = quiz.options[3][2];
-    var opt4 = document.getElementById("op4");
-    opt.innerHTML = quiz.options[3][3];
-
-}
-
-else if (index===4) {
-    var element = document.getElementById("question");
-    element.innerHTML= quiz.questions[4];
-    var opt1 = document.getElementById("op1");
-    opt1.innerHTML = quiz.options[4][0];
-    var opt2 = document.getElementById("op2");
-    opt2.innerHTML = quiz.options[4][1];
-    var opt3 = document.getElementById("op3");
-    opt3.innerHTML = quiz.options[4][2];
-    var opt4 = document.getElementById("op4");
-    opt4.innerHTML = quiz.options[4][3];
-
-}
 function next(){
-    index++;
+  i++;
+  changeContent();
 }
-function pre(){
-    index--;
+// function prev(){
+//   i--;
+//   changeContent();
+// }
+
+function checkAnswer(){
+
 }
