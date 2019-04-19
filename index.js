005ba55d7f97b1b5aca55324ceae37a050142d6c
abc <rajat2502@users.noreply.github.com>
2019-04-19 14:20:55 +0530

    quiz js fix

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ var i = 0;
 
 var score = 0;
 
+var ans;
+
+var options = document.forms[0];
+
 document.getElementById("score").innerHTML = 0;
 document.getElementById("question").innerHTML = quiz.questions[i];
 document.getElementById("op1").innerHTML = quiz.opt1[i];
@@ -29,13 +33,46 @@ function changeContent(){
 
 function next(){
   i++;
+  jQuery("input:radio").attr('disabled',false);
+  jQuery('input:radio').prop('checked', false);
   changeContent();
 }
-// function prev(){
-//   i--;
-//   changeContent();
-// }
 
-function checkAnswer(){
+function checkAnswer(event){
+    if(quiz.opt1[i]==quiz.answer[i]){
+      ans = 1;
+    }
+    else if(quiz.opt2[i]==quiz.answer[i]){
+          ans = 2;
+    }
+    else if(quiz.opt3[i]==quiz.answer[i]){
+          ans = 3;
+    }
+    else if(quiz.opt4[i]==quiz.answer[i]){
+          ans = 4;
+    }
+
+    var whichinput = document.getElementsByName("option");
+    var len = whichinput.length;
+    var checkedinputno;
+    var checkedinput;
+
+    for(var b=0; b<len ; b++){
+      if(whichinput[b].checked){
+        checkedinputno = b;
+        checkedinput = whichinput[b];
+      }
+    }
+
+    if((checkedinputno+1) == ans){
+      scoreUpdate();
+    }
+    jQuery("input:radio").attr('disabled',true);
+}
+
+function scoreUpdate(){
+  score++;
+  document.getElementById("score").innerHTML = score;
+
 
 }
